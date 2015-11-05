504d34fc186e905d9902deffe4b979a236df1797
John Gentle <jgentle@dhcp-129-116-118-113.cem.utexas.edu>
2015-11-05 16:41:06 -0600

    Working on Analysis view anyway - nav text be darned

--- public/modules/dashboard/controllers/dashboard.client.view.controller.js
+++ public/modules/dashboard/controllers/dashboard.client.view.controller.js
@@ -11,7 +11,7 @@
     // This provides Authentication context.
     $scope.authentication = Authentication;
     $scope.currentRoute = 'Dashboard';
-    $scope.listDisplayLimit = 7;
+    $scope.listDisplayLimit = 9;
     $scope.clicked = clicked;
 
     console.log($scope.currentRoute);
